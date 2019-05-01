5bc69cf5f5b5fc508b7b05943c4e0e262cd14ae7
Ori Fogler <ori.fogler@live.biu.ac.il>
2019-05-01 23:42:37 +0300

    init

---
+++ src/Main/Main.java
@@ -0,0 +1,33 @@
+package Main;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.*;
+
+public class Main {
+    public static Frame myFrame;
+
+    public static void main(String[] args) {
+        // gui
+        myFrame = new Frame("Exercise1");
+        MyCanvas myCanvas = new MyCanvas();
+
+        myFrame.add(myCanvas);
+        myFrame.pack();
+        myFrame.setVisible(true);
+        myCanvas.requestFocus();
+
+        myFrame.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+
+        myCanvas.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent componentEvent) {
+                myCanvas.mg.updateWindow(myCanvas.getWidth(), myCanvas.getHeight());
+            }
+        });
+    }
+}
